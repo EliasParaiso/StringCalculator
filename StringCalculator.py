23be2e960b5b908d3768524fe4e009b2a0fe8584
Elias Paraiso <paraiso18@up.edu>
2021-06-06 20:37:10 -0700

    added back comma replacement, changed regex to not filter out negative simple, still need to throw exception on negatives.

--- StringCalculator.py
+++ StringCalculator.py
@@ -10,8 +10,12 @@ def add(numbers):
             return 0        
         
         #replace all non-numeric characters with empty space
-        numbers = re.sub("[^0-9]", " ", numbers)
-        #print(numbers)
+        #numbers = re.sub("[^0-9]", " ", numbers)
+        numbers = re.sub(r'[^-\d]', " ", numbers)
+        print("current string: " + numbers)
+
+        #replace commas with spaces for list conversion
+        numbers = numbers.replace(",", " ")
 
         #convert input to list of strings
         string_list = numbers.split()
@@ -31,26 +35,33 @@ print("expected 0")
 print(add(""))    
 print("expected 3")
 print(add("1,2"))
-"""
+
 
 #part 2 any number of numbers 
-"""
+
 print("expected 449")
 print(add("4 6 55 5 3 34 342"))
-"""
+
 
 #part 3 handle newlines \n
-"""
+
 print("expected 9")
 print(add("3\n3\n3"))
 print("expected 6")
 print(add("1\n2,3"))
 print("expected 0")
 print(add("\n"))
-"""
+
 
 #part 4 handling any delimeter
     #solution - use regex to replace non-numbers with space 
 print("expected 3")
 print(add("//;\n1;2"))
 
+""" 
+
+#part 5 throw exception on negative numbers
+print("expected 3")
+print(add("//;\n1;2"))
+print("expected negatives not allowed")
+print(add("6, -4"))
